a9d756e036008ff52cbee110361782932629b988
Felipe Riveros <feliperiveros123@gmail.com>
2024-06-21 14:22:08 +0200

    finished

--- public/js/app.js
+++ public/js/app.js
@@ -1,85 +1,20 @@
+function ensenaromostrar() {
+  for (let cont = 1; cont < 15; cont++) {
+      let stringid = "box" + cont;
 
+      document.getElementById(stringid).onmouseover = function() { ocultar(cont) };
+      document.getElementById(stringid).onmouseout = function() { ensenar(cont) };
+  }
+}
 
-function ensenaromostrar(){
+function ocultar(cont) {
+  let stringanimal = "emoji" + cont;
+  document.getElementById(stringanimal).style.display = "none";
+}
 
-    for (let cont = 1; cont < 15; cont++) {
-      
-      let stringid ="box"+cont;
-  
-  switch (cont) {
-    case 1:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 2:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 3:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 4:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 5:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 6:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 7:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 8:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 9:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 10:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 11:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 12:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 13:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-      case 14:
-        document.getElementById(stringid).onmouseover = function() {ocultar(cont)};
-        document.getElementById(stringid).onmouseout = function() {ensenar(cont)};
-      break;
-    
-  }
-    }
-  
-  
-  }
-  
-  
-  function ocultar(cont){
-    let stringanimal = "contanimal"+cont;
-    document.getElementById(stringanimal).style.display = "none";
-  
-  }
-  
-  function ensenar(cont){
-    let stringanimal = "contanimal"+cont;
-    document.getElementById(stringanimal).style.display = "block";
-  }
-  
-  ensenaromostrar();
+function ensenar(cont) {
+  let stringanimal = "emoji" + cont;
+  document.getElementById(stringanimal).style.display = "block";
+}
+
+ensenaromostrar();
